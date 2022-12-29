4d5436c1a60ca22ee3f634ca5dd1f879016a3c43
gswirrl <guy@swirrl.com>
2022-12-29 01:40:10 +0000

    demo image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Viewer } from "react-iiif-viewer"
 function App() {
   return (
     <div className="App">
-		<Viewer iiifUrl="https://data.getty.edu/museum/api/iiif/635494/info.json" />
+		<Viewer iiifUrl="/data/info.json" />
     </div>
   );
 }
