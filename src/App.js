749317aec850af2bddfd710a70ea0728bac16606
g j h <guy@swirrl.com>
2022-12-30 07:59:43 +0000

    data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Viewer } from "react-iiif-viewer"
 
-const App = () => 
+const App = ({
+	iiifUrl="data/info.json"
+})	=> 
     <div className="App">
 		<Viewer 
           height="100vh"
           width="100vw"
-          iiifUrl="data/info.json" />
+          iiifUrl={iiifUrl} />
     </div>
 
 export default App;
