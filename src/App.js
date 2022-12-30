51f5ac7ccff426d1fd745c1091999791bba5a3f7
g j h <guy@swirrl.com>
2022-12-30 08:44:28 +0000

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Viewer } from "react-iiif-viewer"
 
-const App = props => 
-    <div className="App" style={{position:"relative"}}>
+const App = props => {
+	const {
+		debug,
+		iiifUrl="data/info.json"
+	} = props
+   return ( <div className="App" style={{position:"relative"}}>
 	<Viewer 
           height="100vh"
           width="100vw"
-          iiifUrl={props.iiifUrl} />
-		 	{ props.debug && props.debug !== "false" && 
+          iiifUrl={iiifUrl} />
+		 	{ debug && debug !== "false" && 
 			<div style={{color:"white", position: "absolute", top:"2rem",left:"2rem",zIndex:"99999999"}}>
 			{JSON.stringify(props)}
 			</div>
 		}
-    </div>
+    </div>)
+}
 
 export default App;
