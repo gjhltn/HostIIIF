3f238c04efdf03edef2a60f606b8adbff2d03e20
gswirrl <guy@swirrl.com>
2023-01-01 02:46:28 +0000

    prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,57 @@
-import babel from '@rollup/plugin-babel'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
-import nodePolyfills from 'rollup-plugin-polyfill-node'
-import terser from '@rollup/plugin-terser'
-import serve from 'rollup-plugin-serve'
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import terser from "@rollup/plugin-terser";
+import serve from "rollup-plugin-serve";
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: false,
-		file: 'build/HostIIIF.js',
-		format: 'iife'
-	},
-	preserveEntrySignatures: 'strict',
-	onwarn: function onwarn(warning, warn) {
-      if (warning.id.indexOf(__dirname + '/node_modules/') === 0) return;
-      warn(warning);
+  input: "src/main.js",
+  output: {
+    sourcemap: false,
+    file: "build/HostIIIF.js",
+    format: "iife",
   },
-	plugins: [	
-		commonjs({
-			include: ['./node_modules/**'],
-			requireReturnsDefault: 'auto'
-		}),
-		nodePolyfills(),
-		nodeResolve({
-			extensions: ['.js'],
-			browser: true,
-			preferBuiltins: true
-		}),
-		replace({
-			preventAssignment: true,
-			'process.env.NODE_ENV': JSON.stringify('development')
-		}),
-		babel({
-			babelHelpers: 'bundled',
-			presets: ['@babel/preset-env', ["@babel/preset-react", { runtime: "automatic" }]],
-			compact: false
-		}),
-		terser(),
-		process.env.NODE_ENV !== 'production' && serve({
-			verbose: true,
-			host: '192.168.1.74',
-			contentBase: 'build',
-			port: 3000,
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Expose-Headers': '*'
-			}
-		})
-	]
-}
+  preserveEntrySignatures: "strict",
+  onwarn: function onwarn(warning, warn) {
+    if (warning.id.indexOf(__dirname + "/node_modules/") === 0) return;
+    warn(warning);
+  },
+  plugins: [
+    commonjs({
+      include: ["./node_modules/**"],
+      requireReturnsDefault: "auto",
+    }),
+    nodePolyfills(),
+    nodeResolve({
+      extensions: [".js"],
+      browser: true,
+      preferBuiltins: true,
+    }),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+    babel({
+      babelHelpers: "bundled",
+      presets: [
+        "@babel/preset-env",
+        ["@babel/preset-react", { runtime: "automatic" }],
+      ],
+      compact: false,
+    }),
+    terser(),
+    process.env.NODE_ENV !== "production" &&
+      serve({
+        verbose: true,
+        host: "192.168.1.74",
+        contentBase: "build",
+        port: 3000,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Expose-Headers": "*",
+        },
+      }),
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Viewer } from "react-iiif-viewer"
+import React from "react";
+import { Viewer } from "react-iiif-viewer";
 
-const App = props => {
-	const {
-		debug,
-		iiifUrl="data/info.json"
-	} = props
-   return ( <div className="App" style={{position:"relative"}}>
-	<Viewer 
-          height="100vh"
-          width="100vw"
-          iiifUrl={iiifUrl} />
-		 	{ debug && debug !== "false" && 
-			<div style={{color:"white", position: "absolute", top:"2rem",left:"2rem",zIndex:"99999999"}}>
-			{JSON.stringify(props)}
-			</div>
-		}
-    </div>)
-}
+const App = (props) => {
+  const { debug, iiifUrl = "data/info.json" } = props;
+  return (
+    <div className="App" style={{ position: "relative" }}>
+      <Viewer height="100vh" width="100vw" iiifUrl={iiifUrl} />
+      {debug && debug !== "false" && (
+        <div
+          style={{
+            color: "white",
+            position: "absolute",
+            top: "2rem",
+            left: "2rem",
+            zIndex: "99999999",
+          }}
+        >
+          {JSON.stringify(props)}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
